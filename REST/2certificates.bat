cc01667b308ec6a03027761d292ae2e42f3f2d9f
Vlad <askadias@mail.ru>
2013-10-09 04:14:06 -0430

    .bat and .sh are not ignored now

--- REST/2certificates.bat
+++
@@ -1,8 +0,0 @@
-keytool -genkeypair -alias cryptoservice -keystore cryptoService.jks -keypass Pa55w0rd4CryprtoService -storepass Pa55w0rd4CryprtoService -dname "CN=localhost, O=forxy, C=RU" -validity 3650
-keytool -genkeypair -alias cryptoclient -keystore cryptoClient.jks -keypass MyCrypt0ClientPa55w0rd -storepass MyCrypt0ClientPa55w0rd -dname "CN=localhost, O=forxy, C=RU" -validity 3650
-
-keytool -export -rfc -alias cryptoservice -keystore cryptoService.jks -storepass Pa55w0rd4CryprtoService -file cryptoServer.cer
-keytool -import -noprompt -trustcacerts -alias cryptoservice -keystore cryptoClient.jks -storepass MyCrypt0ClientPa55w0rd -file cryptoServer.cer
-
-keytool -export -rfc -alias cryptoclient -keystore cryptoClient.jks -storepass MyCrypt0ClientPa55w0rd -file cryptoClient.cer
-keytool -import -noprompt -trustcacerts -alias cryptoclient -keystore cryptoService.jks -storepass Pa55w0rd4CryprtoService -file cryptoClient.cer
--- REST/certificates.bat
+++ REST/certificates.bat
@@ -1,3 +1,4 @@
+:: ---------------------- Crypto Service and Client Certificates ----------------------------------------
 keytool -genkey -v -alias cryptoservice -keyalg RSA -keypass Pa55w0rd4CryprtoService -validity 3650 -dname "CN=localhost, OU=CryptoService, O=Forxy, C=BY" -keystore cryptoService.jks -storepass Pa55w0rd4CryprtoService -storetype JKS
 keytool -genkey -v -alias cryptoclient -keyalg RSA -keypass MyCrypt0ClientPa55w0rd -validity 3650 -dname "CN=Crypto Client, OU=CryptoClient, O=Forxy, C=BY" -keystore cryptoClient.jks -storepass MyCrypt0ClientPa55w0rd -storetype JKS
 keytool -genkey -v -alias cryptoclientweb -keyalg RSA -keypass MyCrypt0ClientPa55w0rd -validity 3650 -dname "CN=Crypto Client Web, OU=CryptoClientWeb, O=Forxy, C=BY" -keystore cryptoClientWeb.p12 -storepass MyCrypt0ClientPa55w0rd -storetype PKCS12
@@ -11,5 +12,5 @@ keytool -import -v -noprompt -alias cryptoclient -keyalg RSA -file cryptoClient.
 keytool -export -rfc -alias cryptoservice -keyalg RSA -keystore cryptoService.jks -storepass Pa55w0rd4CryprtoService -storetype JKS -file cryptoService.cer
 keytool -import -v -noprompt -alias cryptoservice -keyalg RSA -file cryptoService.cer -keystore cryptoClientTrustStore.jks -storepass MyCrypt0ClientPa55w0rd -storetype JKS
 
-
+:: ---------------------- Web Application Certificate ---------------------------------------------------
 keytool -genkey -v -alias web -keyalg RSA -keypass My5ecredWebPa55w0rd -validity 3650 -dname "CN=localhost, OU=Web, O=Forxy, C=BY" -keystore web.jks -storepass My5ecredWebPa55w0rd -storetype JKS
