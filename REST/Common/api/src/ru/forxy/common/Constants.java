9405b80be7d181d6c30562d7ad819069628c7f7b
Vlad <askadias@mail.ru>
2013-10-22 19:44:33 +0300

    Added class for common constants and kundera cassandra support pre implementation

---
+++ REST/Common/api/src/ru/forxy/common/Constants.java
@@ -0,0 +1,8 @@
+package ru.forxy.common;
+
+/**
+ * Basic constants for common usage
+ */
+public abstract class Constants {
+    public static final String SCHEMA_NAME = "forxy";
+}
--- REST/UserService/api/src/ru/forxy/user/pojo/User.java
+++ REST/UserService/api/src/ru/forxy/user/pojo/User.java
@@ -1,24 +1,41 @@
 package ru.forxy.user.pojo;
 
+import ru.forxy.common.Constants;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Arrays;
 import java.util.Date;
 
+
+@Entity
+@Table(name = "user", schema = Constants.SCHEMA_NAME + "@user")
 @XmlRootElement(name = "user")
 public class User {
 
+    @Id
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "password")
     private byte[] password;
 
+    @Column(name = "login")
     private String login;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "first_name")
     private String lastName;
 
+    @Column(name = "gender")
     private Character gender;
 
+    @Column(name = "birth_date")
     private Date birthDate;
 
     public User() {
