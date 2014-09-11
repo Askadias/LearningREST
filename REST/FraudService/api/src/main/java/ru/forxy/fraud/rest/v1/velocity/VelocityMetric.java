e83f11934e513f6fb66fa7146a1cbea8591dfcc3
Vlad <askadias@mail.ru>
2014-09-11 19:38:36 +0300

    Missed commit data

---
+++ REST/FraudService/api/src/main/java/ru/forxy/fraud/rest/v1/velocity/VelocityMetric.java
@@ -0,0 +1,36 @@
+package ru.forxy.fraud.rest.v1.velocity;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Velocity metric row definition
+ */
+@Table(name = "velocity")
+public class VelocityMetric implements Serializable {
+
+    private static final long serialVersionUID = -7204065035725819278L;
+
+    @EmbeddedId
+    private VelocityMetricCompositeKey key;
+    @Column(name = "aggregated_value")
+    private Double aggregatedValue;
+
+    public VelocityMetricCompositeKey getKey() {
+        return key;
+    }
+
+    public void setKey(VelocityMetricCompositeKey key) {
+        this.key = key;
+    }
+
+    public Double getAggregatedValue() {
+        return aggregatedValue;
+    }
+
+    public void setAggregatedValue(Double aggregatedValue) {
+        this.aggregatedValue = aggregatedValue;
+    }
+}
